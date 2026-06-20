6d34649e7272f1f2fa9977cd2074607a65000346
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-06-20 16:48:09 -0300

    Adiciona pagina HTML da entrega do modulo 25

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+    const status = document.getElementById("status");
+
+    status.textContent =
+        "Validação concluída: ambiente pronto para testes mobile com Appium e Android.";
+}
